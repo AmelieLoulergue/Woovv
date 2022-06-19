fdc615a8d8298bdb355d19fef6c993ee9c804695
AmelieLoulergue <loulergueamelie@yahoo.fr>
2022-06-19 11:15:39 +0200

    fix sendgrid

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
 class ApplicationMailer < ActionMailer::Base
-  default from: 'hello@woovv.io'
+  default from: 'loulergueamelie@yahoo.fr'
   layout 'mailer'
 end
--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -6,6 +6,6 @@ class ContactMailer < ApplicationMailer
     @email = @contact.email
     @object = @contact.object
     @content = @contact.content
-    mail(to: 'hello@woovv.io', subject: "Un Woovver nous sollicite !")
+    mail(to: 'loulergueamelie@yahoo.fr', subject: "Un Woovver nous sollicite !")
   end
 end
